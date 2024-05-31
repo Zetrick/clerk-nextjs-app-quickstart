33f65572b0550674c27ca3d96f3f1717c64c3259
Zach Hetrick <Zetrick@gmail.com>
2024-05-31 16:21:17 -0700

    small tailwind tweak

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,15 +24,17 @@ export default function RootLayout({
                 <p className="bg-green-800 w-full text-center font-extrabold tracking-widest select-none py-4">
                   You are signed in!
                 </p>
-                <div className=" flex justify-center items-center bg-green-600 tracking-wider w-32 absolute right-0 md:h-full font-bold ">
-                  <SignOutButton />
+                <div className="flex flex-col md:flex-row absolute right-0 h-full">
+                  <Link
+                    className=" flex justify-center items-center bg-green-700 tracking-wider w-32 right-32 h-full font-bold"
+                    href={"/dashboard"}
+                  >
+                    Dashboard
+                  </Link>
+                  <div className=" flex justify-center items-center bg-green-600 tracking-wider w-32 right-0 h-full font-bold ">
+                    <SignOutButton />
+                  </div>
                 </div>
-                <Link
-                  className=" flex justify-center items-center bg-green-700 tracking-wider w-32 absolute right-32 h-full font-bold"
-                  href={"/dashboard"}
-                >
-                  Dashboard
-                </Link>
               </div>
             </SignedIn>
             <SignedOut>
